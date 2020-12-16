6f82ad4e99b703644df55cbeff8203148cd04dbb
Leonid Belousov <leonid19992812strelok@Gmail.com>
2020-12-16 23:39:23 +0500

    remove oidc config and remove middleware

--- middleware/oidc-callback.js
+++
@@ -1,6 +0,0 @@
-export default async function ({ store, redirect, route }) {
-    if (route.path == "/callback") {
-      let redirectPath = await store.dispatch("oidc/oidcSignInCallback");
-      redirect(redirectPath);
-    }
-  }
